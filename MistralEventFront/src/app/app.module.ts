f6908b2db9d08e657c269c23e52f498a71cfb716
Nicola Carraro <carraron985@gmail.com>
2021-03-01 22:47:32 +0100

    login component

--- MistralEventFront/src/app/app.module.ts
+++ MistralEventFront/src/app/app.module.ts
@@ -5,17 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-   /*  AlertModule.forRoot() */
+    AppRoutingModule,
+
+    FormsModule
+
+    /*  AlertModule.forRoot() */
   ],
   providers: [],
   bootstrap: [AppComponent]
