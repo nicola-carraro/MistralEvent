c9f7b09060748f17fadfb43421a8f6bc46238248
Christophe FIRPO <christophe.firpo@gmail.com>
2021-03-03 18:35:19 +0100

    Auto stash before merge of "WORK-CFI" and "origin/WORK-APF"

--- MistralEventFront/src/app/app.module.ts
+++ MistralEventFront/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AccountComponent } from './components/account/account.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
---
+++ MistralEventFront/src/app/components/credit/credit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-credti',
+  templateUrl: './credit.component.html',
+  styleUrls: ['./credit.component.scss']
+})
+export class CreditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
