e5800dc89d612d9ba3d0903b9d50fafc1d850c83
AnthonyPFEIFER <swat.wrandrall@gmail.com>
2021-03-06 00:06:28 +0100

    Ajout et mise en place du composant Agenda

--- MistralEventFront/src/app/app-routing.module.ts
+++ MistralEventFront/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AccountComponent } from './components/account/account.component';
+import { AgendaComponent } from './components/agenda/agenda.component';
 import { CreditComponent } from './components/credit/credit.component';
 
 const routes: Routes = [
   { path: 'account', component: AccountComponent },
-  { path: 'credit', component: CreditComponent }
+  { path: 'credit', component: CreditComponent },
+  { path: 'agenda', component: AgendaComponent}
 ];
 
 @NgModule({
