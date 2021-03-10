ea122b4ac16eb7fbf88510b64578fe3217426506
AnthonyPfeifer <anthony.pfeifer@mistral.fr>
2021-03-10 09:56:09 +0100

    Modifs nav et sidebar

--- MistralEventFront/src/app/services/evenement.service.ts
+++ MistralEventFront/src/app/services/evenement.service.ts
@@ -11,7 +11,7 @@ export class EvenementService {
     {
       id: 1,
       name: 'test1',
-      date: new Date('10 Mars, 2021 15:00:00'),
+      date: new Date(),
       description:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
       type: 'resto',
@@ -19,7 +19,7 @@ export class EvenementService {
     {
       id: 2,
       name: 'test2',
-      date: new Date('22 Mars, 2021 15:00:00'),
+      date: new Date(),
       description:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
       type: 'rando',
@@ -27,7 +27,7 @@ export class EvenementService {
     {
       id: 3,
       name: 'test3',
-      date: new Date('25 Mars, 2021 15:00:00'),
+      date: new Date(),
       description:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
       type: 'resto',
@@ -35,7 +35,7 @@ export class EvenementService {
     {
       id: 4,
       name: 'test4',
-      date: new Date('28 Mars, 2021 15:00:00'),
+      date: new Date(),
       description:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
       type: 'apéro',
